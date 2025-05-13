17a7d456109b756d258693e03f6c8b9e564f778b
Fábio Costa <fabiodanielmc@gmail.com>
2025-05-13 11:38:08 +0200

    changed the styling on interest as well as text

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,31 +1,75 @@
 import React from "react";
 
 const Interests = () => (
-  <section id="interests" className="min-h-screen flex flex-col justify-center items-center bg-fixed bg-cover bg-center" style={{ backgroundImage: 'url(/path/to/your/background-image.jpg)' }}>
-    <h3 className="text-4xl mb-4 text-gray-900">I have done a few cool things</h3>
-    <ul className="text-lg text-gray-700 space-y-3">
-      <li className="flex items-center space-x-4">
-        <img src="/src/assets/Manta5.jpg" alt="Reworking bikes" className="w-16 h-16 object-cover" />
-        <span>In October 2023, I went to Slovenia to rework electric hydrofoiling bikes for <a href="https://manta5.com/">Manta5</a>. That was cool!</span>
-      </li>
-      <li className="flex items-center space-x-4">
-        <img src="/src/assets/Manta5.jpg" alt="Endurance sports" className="w-16 h-16 object-cover" />
-        <span>And now I've been doing endurance sports.</span>
-      </li>
-      <li className="flex items-center space-x-4">
-        <img src="/src/assets/climbing.png" alt="Climbing" className="w-16 h-16 object-cover" />
-        <span>I have done some climbing.</span>
-      </li>
-      <h3 className="text-4xl mb-4 text-gray-900">But I am still a nerd</h3>
-      <li className="flex items-center space-x-4">
-        <img src="/src/assets/boardgames.jpg" alt="Board games" className="w-16 h-16 object-cover" />
-        <span>I enjoy board games and computer building.</span>
-      </li>
-      <li className="flex items-center space-x-4">
-        <img src="/src/assets/hiking.jpg" alt="Plants" className="w-16 h-16 object-cover" />
-        <span>I have a passion for nature. I enjoy hiking and gardening.</span>
-      </li>
-    </ul>
+  <section id="interests" className="min-h-screen flex flex-col items-center bg-fixed bg-cover bg-center" style={{ backgroundImage: 'url(/path/to/your/background-image.jpg)' }}>
+    <h2 className="text-4xl mb-4 p-4 text-gray-900">I have done a few cool things</h2>
+    <div className="flex flex-col items-center w-full px-4">  {/* Added padding on the left and right */}
+      <div className="flex flex-row items-center w-full mt-8">
+        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 p-4">
+          <img src="/src/assets/climbing.png" alt="Climbing" className="w-full h-full object-cover rounded-lg" />
+        </div>
+        <div className="h-[50vh] rounded-lg bg-white shadow-lg flex flex-col justify-end p-6 w-1/2">
+          <h3 className="p-8 text-8xl font-bold text-gray-900">Some climbing. 🧗‍♂️</h3>
+          <p className="p-8 text-5xl text-gray-700">Look at me, trying to not look down 😨 </p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center w-full mt-8">
+        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 p-4">
+          <img src="/src/assets/Manta5.jpg" alt="Endurance sports" className="w-full h-full object-cover rounded-lg" />
+        </div>
+        <div className="h-[50vh] rounded-lg bg-white shadow-lg flex flex-col justify-end p-6 w-1/2">
+          <h3 className="p-8 text-8xl font-bold text-gray-900">I've been doing endurance sports.</h3>
+          <p className="p-8 text-5xl text-gray-700">Started witj cycling, but trying more stuff 🏊‍♂️ 🚴‍♂️ 🏃‍♂️</p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center w-full mt-8">
+        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 p-4">
+          <img src="/src/assets/Manta5.jpg" alt="Reworking bikes" className="w-full h-full object-cover rounded-lg" />
+        </div>
+        <div className="h-[50vh] rounded-lg bg-white shadow-lg flex flex-col justify-end p-6 w-1/2">
+          <h3 className="p-8 text-8xl font-bold text-gray-900">In October 2023, I went to Slovenia </h3>
+          <p className="p-8 text-gray-700 text-5xl">To rework electric hydrofoiling bikes for <a href="https://manta5.com/" className="text-blue-500 hover:text-blue-700 transition duration-300 transform hover:scale-105">Manta5</a>. That was cool!</p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center w-full mt-8">
+        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 p-4">
+          <img src="/src/assets/hiking.jpg" alt="Plants" className="w-full h-full object-cover rounded-lg" />
+        </div>
+        <div className="h-[50vh] rounded-lg bg-white shadow-lg flex flex-col justify-end p-6 w-1/2">
+          <h3 className="p-8 text-8xl font-bold text-gray-900">I have a passion for nature. </h3>
+          <p className="p-8 text-5xl text-gray-700"> I enjoy hiking and gardening.🏞️ 🌱 </p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center w-full mt-8">
+        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 p-4">
+          <img src="/src/assets/Shelves.jpg" alt="Plants" className="w-full h-full object-cover rounded-lg" />
+        </div>
+        <div className="h-[50vh] rounded-lg bg-white shadow-lg flex flex-col justify-end p-6 w-1/2">
+          <h3 className="p-8 text-8xl font-bold text-gray-900">I like building things with my hands. </h3>
+          <p className="p-8 text-5xl text-gray-700"> Yes, I actually built those shelves 😲 🪚 </p>
+        </div>
+      </div>
+    </div>
+    <h3 className="text-4xl mb-4 text-gray-900">But I am still a nerd at heart</h3>
+    <div className="flex flex-col items-center w-full mt-8">
+      <div className="flex flex-row items-center w-full">
+        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 p-4">
+          <img src="/src/assets/boardgames.jpg" alt="Board games" className="w-full h-full object-cover rounded-lg" />
+        </div>
+        <div className="h-[50vh] rounded-lg bg-white shadow-lg flex flex-col justify-end p-6 w-1/2">
+          <h3 className="p-8 text-8xl font-bold text-gray-900">I enjoy board games and computer building.</h3>
+        </div>
+      </div>
+      <div className="flex flex-row items-center w-full mt-8">
+        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 p-4">
+          <img src="/src/assets/Keyboard.jpg" alt="Board games" className="w-full h-full object-cover rounded-lg" />
+        </div>
+        <div className="h-[50vh] rounded-lg bg-white shadow-lg flex flex-col justify-end p-6 w-1/2">
+          <h3 className="p-8 text-8xl font-bold text-gray-900">And I am trying to learn music production.</h3>
+          <p className="p-8 text-5xl text-gray-700"> I am not very good at it, but it is fun!🕺</p>
+        </div>
+      </div>
+    </div>
   </section>
 );
 
