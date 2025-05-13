0581b7c08d6de38ec3ab94717fdce699337736f0
Fábio Costa <fabiodanielmc@gmail.com>
2025-05-13 12:40:36 +0200

    changed padding on interest section, still cannot see experience

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -4,8 +4,8 @@ const Interests = () => (
   <section id="interests" className="min-h-screen flex flex-col items-center bg-fixed bg-cover bg-center" style={{ backgroundImage: 'url(/path/to/your/background-image.jpg)' }}>
     <h2 className="p-8 text-8xl font-bold text-gray-900">I have done a few cool things</h2>
     <div className="flex flex-col items-center w-full px-4">  {/* Added padding on the left and right */}
-      <div className="flex flex-row items-center w-full mt-8">
-        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 p-4">
+      <div className="flex flex-row items-center w-full mt-8 px-20">
+        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 px-8">
           <img src="/src/assets/climbing.png" alt="Climbing" className="w-full h-full object-cover rounded-lg" />
         </div>
         <div className="h-[50vh] rounded-lg bg-white shadow-lg flex flex-col justify-end p-6 w-1/2">
@@ -13,8 +13,8 @@ const Interests = () => (
           <p className="p-8 text-5xl text-gray-700">Look at me, trying to not look down 😨 </p>
         </div>
       </div>
-      <div className="flex flex-row items-center w-full mt-8">
-        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 p-4">
+      <div className="flex flex-row items-center w-full mt-8 px-20">
+        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 px-8">
           <img src="/src/assets/Manta5.jpg" alt="Endurance sports" className="w-full h-full object-cover rounded-lg" />
         </div>
         <div className="h-[50vh] rounded-lg bg-white shadow-lg flex flex-col justify-end p-6 w-1/2">
@@ -22,8 +22,8 @@ const Interests = () => (
           <p className="p-8 text-5xl text-gray-700">Started witj cycling, but trying more stuff 🏊‍♂️ 🚴‍♂️ 🏃‍♂️</p>
         </div>
       </div>
-      <div className="flex flex-row items-center w-full mt-8">
-        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 p-4">
+      <div className="flex flex-row items-center w-full mt-8 px-20">
+        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 px-8">
           <img src="/src/assets/Manta5.jpg" alt="Reworking bikes" className="w-full h-full object-cover rounded-lg" />
         </div>
         <div className="h-[50vh] rounded-lg bg-white shadow-lg flex flex-col justify-end p-6 w-1/2">
@@ -31,8 +31,8 @@ const Interests = () => (
           <p className="p-8 text-gray-700 text-5xl">To rework electric hydrofoiling bikes for <a href="https://manta5.com/" className="text-blue-500 hover:text-blue-700 transition duration-300 transform hover:scale-105">Manta5</a>. That was cool!</p>
         </div>
       </div>
-      <div className="flex flex-row items-center w-full mt-8">
-        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 p-4">
+      <div className="flex flex-row items-center w-full mt-8 px-20">
+        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 px-8">
           <img src="/src/assets/hiking.jpg" alt="Plants" className="w-full h-full object-cover rounded-lg" />
         </div>
         <div className="h-[50vh] rounded-lg bg-white shadow-lg flex flex-col justify-end p-6 w-1/2">
@@ -40,8 +40,8 @@ const Interests = () => (
           <p className="p-8 text-5xl text-gray-700"> I enjoy hiking and gardening.🏞️ 🌱 </p>
         </div>
       </div>
-      <div className="flex flex-row items-center w-full mt-8">
-        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 p-4">
+      <div className="flex flex-row items-center w-full mt-8 px-20">
+        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 px-8">
           <img src="/src/assets/Shelves.jpg" alt="Plants" className="w-full h-full object-cover rounded-lg" />
         </div>
         <div className="h-[50vh] rounded-lg bg-white shadow-lg flex flex-col justify-end p-6 w-1/2">
@@ -50,18 +50,18 @@ const Interests = () => (
         </div>
       </div>
     </div>
-    <h3 className="text-4xl mb-4 text-gray-900">But I am still a nerd at heart</h3>
-    <div className="flex flex-col items-center w-full mt-8">
+    <div className="flex flex-col items-center w-full mt-8 px-20">
       <div className="flex flex-row items-center w-full">
-        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 p-4">
+        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 px-8">
           <img src="/src/assets/boardgames.jpg" alt="Board games" className="w-full h-full object-cover rounded-lg" />
         </div>
         <div className="h-[50vh] rounded-lg bg-white shadow-lg flex flex-col justify-end p-6 w-1/2">
-          <h3 className="p-8 text-8xl font-bold text-gray-900">I enjoy board games and computer building.</h3>
+          <h3 className="p-8 text-8xl font-bold text-gray-900">I enjoy games and computer building.</h3>
+          <p className="p-8 text-5xl text-gray-700"> I am still a nerd at heart ❤️</p>
         </div>
       </div>
-      <div className="flex flex-row items-center w-full mt-8">
-        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 p-4">
+      <div className="flex flex-row items-center w-full mt-8 px-20">
+        <div className="h-[50vh] rounded-lg overflow-hidden w-1/2 px-8">
           <img src="/src/assets/Keyboard.jpg" alt="Board games" className="w-full h-full object-cover rounded-lg" />
         </div>
         <div className="h-[50vh] rounded-lg bg-white shadow-lg flex flex-col justify-end p-6 w-1/2">
